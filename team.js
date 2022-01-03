cd8d368a7a83e102766e84ffb168f59c6340ae9b
jakub wytykowski <jakub.wytykowski@gmail.com>
2022-01-03 22:15:07 +0100

    suitable for check

---
+++ team.js
@@ -0,0 +1,27 @@
+// section controlling appearing navbar sticky and button to scroll
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+        $('.navbar').addClass("sticky");
+    }else{
+        $('.navbar').removeClass("sticky");
+    }
+    if(this.scrollY > 200){
+        $('.scroll-up-btn').addClass("show");
+    }else{
+        $('.scroll-up-btn').removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+      // removing smooth scroll on slide-up button click quicker moving
+      $('html').css("scrollBehavior", "auto");
+  });
+
+  $('.navbar .menu li a').click(function(){
+      // applying again smooth scroll on menu items click
+      $('html').css("scrollBehavior", "smooth");
+  });
+});
